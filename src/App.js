af03317a80ac38dd3494a0f0b850fff6a1972041
ahmed61289 <ahmed61289@gmail.com>
2020-06-03 14:45:49 +0500

    Changes in App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <div>
+            Created by <b>AHMED ASLAM</b>
+          </div>
         </p>
         <a
           className="App-link"
